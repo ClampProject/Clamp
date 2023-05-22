1463abe36745f9f12bc5d491a0eadd60ed12c0ba
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-05-21 20:30:19 -0600

    Update +error.svelte

--- svelte-app/src/routes/+error.svelte
+++ svelte-app/src/routes/+error.svelte
@@ -1,2 +1,8 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
 <h1>Whoops!</h1>
 <p>An error occurred. What a disappointment that is!</p>
+
+<p>Error {$page.status}: {$page.error.message}</p>
