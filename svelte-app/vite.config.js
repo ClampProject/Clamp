40c211b2dac336ef06a349a98e66bbbf60bcab35
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-06-12 15:14:13 -0600

    wgrhgesfd vercel; part 10

--- svelte-app/vite.config.js
+++ svelte-app/vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	optimizeDeps: {
 		include: [
-			// '@blockly/continuous-toolbox',
-			// 'file-dialog'
+			'@blockly/continuous-toolbox',
+			'file-dialog'
 		],
 		exclude: []
 	},
