280c3b3bc1cb1bc70dbec40e34bb8a9a6933007b
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2024-02-03 16:23:16 -0700

    fix a few toolbox bugs

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -88,6 +88,62 @@
         },
     };
 
+    // patch Blockly to fix zooming with the toolbox
+    Blockly.VerticalFlyout.prototype.getFlyoutScale = function() {
+      return config.zoom.startScale;
+    };
+    // patch Blockly to add a max width to the toolbox
+    // note, some code for handling RTL is removed here
+    // some code for something about Workspace with no scrollbars where this is permanently open on the left. is also removed
+    Blockly.VerticalFlyout.prototype.reflowInternal_ = function() {
+      this.workspace_.scale = this.getFlyoutScale();
+      let flyoutWidth = 0;
+      const blocks = this.workspace_.getTopBlocks(false);
+      for (let i = 0, block; (block = blocks[i]); i++) {
+        let width = block.getHeightWidth().width;
+        if (block.outputConnection) {
+          width -= this.tabWidth_;
+        }
+        flyoutWidth = Math.max(flyoutWidth, width);
+      }
+      for (let i = 0, button; (button = this.buttons_[i]); i++) {
+        flyoutWidth = Math.max(flyoutWidth, button.width);
+      }
+      flyoutWidth += this.MARGIN * 1.5 + this.tabWidth_;
+      flyoutWidth *= this.workspace_.scale;
+      flyoutWidth += Blockly.Scrollbar.scrollbarThickness;
+      
+      flyoutWidth = Math.min(
+        flyoutWidth,
+        250
+      );
+
+      if (this.width_ !== flyoutWidth) {
+        for (let i = 0, block; (block = blocks[i]); i++) {
+          if (this.rectMap_.has(block)) {
+            this.moveRectToBlock_(this.rectMap_.get(block), block);
+          }
+        }
+        
+        // if (
+        //   !this.targetWorkspace.scrollbar &&
+        //   !this.autoClose &&
+        //   this.targetWorkspace.getFlyout() === this &&
+        //   this.toolboxPosition_ === toolbox.Position.LEFT
+        // ) {
+        //   this.targetWorkspace.translate(
+        //     this.targetWorkspace.scrollX + flyoutWidth,
+        //     this.targetWorkspace.scrollY,
+        //   );
+        // }
+        
+        this.width_ = flyoutWidth;
+        this.position();
+        this.targetWorkspace.resizeContents();
+        this.targetWorkspace.recordDragTargets();
+      }
+    };
+
     let workspace;
     let compiler;
     let lastGeneratedCode = "";
@@ -481,7 +537,7 @@
                         blocklyToolboxIsVisible = !blocklyToolboxIsVisible;
                         workspace
                             .getFlyout()
-                            .setVisible(blocklyToolboxIsVisible);
+                            .setContainerVisible(blocklyToolboxIsVisible);
                     }}
                     class="hide-toolbox"
                 >
