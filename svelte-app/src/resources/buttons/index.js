df9e678c6abf10a091fe01b3266d20068a29ca35
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2024-02-05 08:17:22 +0000

    e

--- svelte-app/src/resources/buttons/index.js
+++ svelte-app/src/resources/buttons/index.js
@@ -1,7 +1,9 @@
 import Blockly from 'blockly/core'
 
 export default () => {
+    /* do to
     Blockly.Workspace.registerButtonCallback("buttonPlaceholder", () => {
         alert('coming soon!')
     })
+    */
 }
