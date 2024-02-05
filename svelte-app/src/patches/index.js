bb4e55a743f10d8bcfc7615dfac620dc08bf8588
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2024-02-05 21:10:28 +0000

    my bad

--- svelte-app/src/patches/index.js
+++ svelte-app/src/patches/index.js
@@ -1,5 +1,6 @@
 import ToolboxFlyout from './blockly/toolbox-flyout';
 import FlyoutButton from './blockly/flyout-button';
+import Renderer from './blockly/renderer';
 
 export default {
     Blockly: {
