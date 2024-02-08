3d3f975c88dc9118ae1007e911a5a28fb5c1fba4
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2024-02-08 06:31:07 +0000

    function category execute

--- svelte-app/src/resources/blocks/functions.js
+++ svelte-app/src/resources/blocks/functions.js
@@ -24,6 +24,41 @@ function register() {
         const BLOCKS = javascriptGenerator.statementToCode(block, 'BLOCKS');
         return [`(async () => { ${BLOCKS} })`, javascriptGenerator.ORDER_NONE];
     })
+    // execute
+    registerBlock(`${categoryPrefix}execute_a`, {
+        message0: 'execute %1',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "FUNC",
+                "check": "Function"
+            }
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const FUNC = javascriptGenerator.valueToCode(block, 'FUNC', javascriptGenerator.ORDER_ATOMIC);
+        const code = `${FUNC}();`;
+        return `${code}\n`;
+    })
+    registerBlock(`${categoryPrefix}execute_b`, {
+        message0: 'execute %1',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "FUNC",
+                "check": "Function"
+            }
+        ],
+        output: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const FUNC = javascriptGenerator.valueToCode(block, 'FUNC', javascriptGenerator.ORDER_ATOMIC);
+        return [`(${FUNC}())`, javascriptGenerator.ORDER_NONE];
+    })
 }
 
 export default register;
